5fc8b8dfb2d1b9f417fc6d9ee6f8ab76f0b4a49f
pranav_vohra <pranavvohra1812@yahoo.in>
2023-01-13 12:34:12 +0530

    chore: javascripting project setup and completed some basic problems

---
+++ problems/function-arguments.js
@@ -0,0 +1,6 @@
+const math= (n1, n2, n3)=> {
+    return n1+ n2 *n3;
+}
+
+console.log(math(53, 61, 67));
+
---
+++ problems/if-statement.js
@@ -0,0 +1,3 @@
+var fruit= "orange";
+
+console.log(`The fruit name has ${fruit.length>5? "more" : "less than/equal to"} 5 letters`)
